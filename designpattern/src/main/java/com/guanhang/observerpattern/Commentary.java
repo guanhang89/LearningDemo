1cd920c490f7bb7fe02dea33378edb400cb5732c
guanhang <827360689@qq.com>
2019-01-08 23:47:48 +0800

    observer pattern

---
+++ designpattern/src/main/java/com/guanhang/observerpattern/Commentary.java
@@ -0,0 +1,5 @@
+package com.guanhang.observerpattern;
+
+public interface Commentary {
+	public void setDesc(String desc);
+}
---
+++ designpattern/src/main/java/com/guanhang/observerpattern/Observer.java
@@ -0,0 +1,7 @@
+package com.guanhang.observerpattern;
+
+public interface Observer {
+	public void update(String desc);
+	public void subscribe();
+	public void unSubscribe();
+}
